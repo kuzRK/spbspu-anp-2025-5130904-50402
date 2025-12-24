c99469956945649f50b662dff15b25252785570f
kuzRK <sanalsogdanov11188@gmail.com>
2025-12-24 18:56:15 +0300

    scaleFromPoint totalFrame output

--- sogdanov.sanal/P5/main.cpp
+++ sogdanov.sanal/P5/main.cpp
@@ -53,6 +53,13 @@ namespace sogdanov
     double diagy_;
     point_t pos_;
   };
+  void scaleFromPoint(Shape * shps[], double k, size_t size, point_t pt);
+  double leftX(const rectangle_t& r);
+  double rightX(const rectangle_t& r);
+  double botY(const rectangle_t& r);
+  double topY(const rectangle_t& r);
+  rectangle_t totalFrame(Shape * shps[], size_t size);
+  void output(Shape * shps[], size_t size);
 }
 int main()
 {}
@@ -63,20 +70,20 @@ double sogdanov::Rectangle::getArea() const
 {
   return width_ * height_;
 }
-sogdanov::rectangle_t sogdanov::Rectangle::getFrameRect() const override
+sogdanov::rectangle_t sogdanov::Rectangle::getFrameRect() const
 {
   return sogdanov::rectangle_t{width_, height_, pos_};
 }
-void sogdanov::Rectangle::move(const point_t p) override
+void sogdanov::Rectangle::move(const point_t p)
 {
   pos_ = p;
 }
-void sogdanov::Rectangle::move(double dx, double dy) override
+void sogdanov::Rectangle::move(double dx, double dy)
 {
   pos_.x += dx;
   pos_.y += dy;
 }
-void sogdanov::Rectangle::scale(double k) override
+void sogdanov::Rectangle::scale(double k)
 {
   width_ *= k;
   height_ *= k;
@@ -84,49 +91,114 @@ void sogdanov::Rectangle::scale(double k) override
 sogdanov::Xquare::Xquare(double d, point_t p):
   d_(d), pos_(p)
 {}
-double getArea() const override
+double sogdanov::Xquare::getArea() const
 {
   return (d_ * d_) / 2.0;
 }
-sogdanov::rectangle_t sogdanov::Xquare::getFrameRect() const override
+sogdanov::rectangle_t sogdanov::Xquare::getFrameRect() const
 {
   return sogdanov::rectangle_t{d_, d_, pos_};
 }
-void sogdanov::Xquare::move(double dx, double dy) override
+void sogdanov::Xquare::move(double dx, double dy)
 {
   pos_.x += dx;
   pos_.y += dy;
 }
-void sogdanov::Xquare::move(point_t p) override
+void sogdanov::Xquare::move(point_t p)
 {
   pos_ = p;
 }
-void sogdanov::Xquare::scale(double k) override
+void sogdanov::Xquare::scale(double k)
 {
   d_ *= k;
 }
 sogdanov::Diamond::Diamond(double diagx, double diagy, point_t p):
   diagx_(diagx), diagy_(diagy), pos_(p);
 {}
-double sogdanov::Diamond::getArea() const override
+double sogdanov::Diamond::getArea() const
 {
   return (diagx_ * diagy_) / 2.0;
 }
-sogdanov::rectangle_t sogdanov::Diamond::getFrameRect() const override
+sogdanov::rectangle_t sogdanov::Diamond::getFrameRect() const
 {
   return sogdanov::rectangle_t{diagx_, diagy_, pos_};
 }
-void sogdanov::Diamond::move(double dx, double dy) override
+void sogdanov::Diamond::move(double dx, double dy)
 {
   pos_.x += dx;
   pos_.y += dy;
 }
-void sogdanov::Diamond::move(const point_t p) override
+void sogdanov::Diamond::move(const point_t p)
 {
   pos_ = p;
 }
-void sogdanov::Diamond::scale(double k) override
+void sogdanov::Diamond::scale(double k)
 {
   diagx *= k;
   diagy *= k;
+}
+void sogdanov::scaleFromPoint(Shape * shps[], double k, size_t size, point_t pt)
+{
+  for (size_t i = 0; i < size; ++i) {
+    shps[i]->move(-pt.x, -pt.y);
+    shps[i]->scale(k);
+    shps[i]->move(pt.x, pt.y);
+  }
+}
+double sogdanov::leftX(const rectangle_t& r)
+{
+  return r.pos.x - r.width / 2.0;
+}
+double sogdanov::rightX(const rectangle_t& r)
+{
+  return r.pos.x + r.width / 2.0;
+}
+double sogdanov::botY(const rectangle_t& r)
+{
+  return r.pos.y - r.height / 2.0;
+}
+double sogdanov::topY(const rectangle_t& r)
+{
+  return r.pos.y + r.height / 2.0;
+}
+sogdanov::rectangle_t sogdanov::totalFrame(Shape * shps[], size_t size)
+{
+  sogdanov::rectangle_t cur = shapes[0]->getFrameRect();
+  double left = leftX(cur);
+  double right = rightX(cur);
+  double bottom = botY(cur);
+  double top = topY(cur);
+  for (size_t i = 1; i < size; ++i) {
+    sogdanov::rectangle_t r = shapes[i]->getFrameRect();
+    left = std::min(left, leftX(r));
+    right = std::max(right, rightX(r));
+    bottom = std::min(bottom, botY(r));
+    top = std::max(top, topY(r));
+  }
+  sogdanov::rectangle_t total;
+  total.width = right - left;
+  total.height = top - bottom;
+  total.pos.x = (left + right) / 2.0;
+  total.pos.y = (bottom + top) / 2.0;
+  return total;
+}
+void sogdanov::output(Shape * shps[], size_t size)
+{
+  double total_area = 0.0;
+  for (size_t i = 0; i < size; ++i) {
+    double area = shapes[i]->getArea();
+    total_area += area;
+    std::cout << area << "\n";
+    rectangle_t frame = shapes[i]->getFrameRect();
+    std::cout << frame.pos.x << "\n";
+    std::cout << frame.pos.y << "\n";
+    std::cout << frame.width << "\n";
+    std::cout << frame.height << "\n";
+  }
+  rectangle_t total_frame = TotalFrame(shapes, size);
+  std::cout << total_area << "\n";
+  std::cout << total_frame.pos.x << "\n";
+  std::cout << total_frame.pos.y << "\n";
+  std::cout << total_frame.width << "\n";
+  std::cout << total_frame.height << "\n";
 }
